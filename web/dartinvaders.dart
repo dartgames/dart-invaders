797f0ebbe65a207524f736f2a75e9f36826c8b5e
William Oliveira <sudowilliam@gmail.com>
2014-09-13 01:16:05 -0300

    Initial commit

---
+++ web/dartinvaders.dart
@@ -0,0 +1,31 @@
+import 'dart:html';
+import 'dart:async';
+
+const String BACKGROUND = "images/background.png";
+
+CanvasElement canvas;
+
+ImageElement background;
+
+void update(Timer t) {
+    draw();
+}
+
+void init() {
+    background = new ImageElement();
+    background.src = BACKGROUND;
+}
+
+void draw() {
+    CanvasRenderingContext2D canvasContext = canvas.context2D;
+    
+    canvasContext.drawImage(background, 0, 0);
+}
+
+void main() {
+    canvas = querySelector("#dartInvaders");
+    
+    init();
+    
+    Timer t = new Timer.periodic(const Duration(milliseconds: 17), update);
+}
